c845112881ef2bea55cf406baae4a9467b5679a6
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-13 18:32:54 +0530

    Added categories

--- app_idx.js
+++ app_idx.js
@@ -3,4 +3,18 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+
+const categories = [
+    {id: 1, name: "Front-End Development"},
+    {id: 2, name: "Back-End Development"},
+    {id: 3, name: "Full-Stack Development"},
+    {id: 4, name: "Mobile App Development"},
+    {id: 5, name: "Machine Learning"},
+    {id: 6, name: "UI / UX Design"},
+    {id: 7, name: "Block-Chain Technology"},
+    {id: 8, name: "Cyber Security"},
+    {id: 9, name: "Operating System Design"},
+]
 
