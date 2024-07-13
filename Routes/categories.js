11c8acf9d3a11eff6e4d01fea06edb9023645c81
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-13 21:32:43 +0530

    Added the CRUD methods into category route

--- Routes/categories.js
+++ Routes/categories.js
@@ -1,1 +1,55 @@
 
+
+
+
+const categories = [
+    { id: 1, name: "Front-End Development" },
+    { id: 2, name: "Back-End Development" },
+    { id: 3, name: "Full-Stack Development" },
+    { id: 4, name: "Mobile App Development" },
+    { id: 5, name: "Machine Learning" },
+    { id: 6, name: "UI / UX Design" },
+    { id: 7, name: "Block-Chain Technology" },
+    { id: 8, name: "Cyber Security" },
+    { id: 9, name: "Operating System Design" },
+]
+
+app.get('/api/categories', (req, res) => {
+    res.send(categories);
+});
+
+app.post('/api/categories', (req, res) => {
+    const categ = { id: categories.length + 1, name: req.body.name };
+    categories.push(categ);
+    res.send(categ);
+});
+
+
+app.put('/api/categories/:id', (req, res) => {
+    const categ = categories.find( (c) => c.id === parseInt(req.params.id) );
+    if (!categ) return res.status(404).send('Category with given ID is not found..');
+
+    if (error) return res.status(404).send(error.details[0].message);
+
+    categ.name = req.body.name;
+    res.send(categ);
+});
+
+
+app.delete('/api/categories/:id', (req, res) => {
+    const categ = categories.find( (c) => c.id === parseInt(req.params.id) );
+    if (!categ) return res.status(404).send('Category with given ID is not found..');
+
+    const idx = categories.indexOf(categ);
+    categories.splice(idx, 1);
+
+    res.send(categ);
+});
+
+
+app.get('/api/categories/:id', (req, res) => {
+    const categ = categories.find( (c) => c.id === parseInt(req.params.id) );
+    if (!categ) return res.status(404).send('Category with given ID is not found..');
+    res.send(categ);
+});
+
