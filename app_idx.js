46da87b477d6e7cc078834b8d167bd76928ef28e
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-13 18:38:27 +0530

    Added a get method

--- app_idx.js
+++ app_idx.js
@@ -7,14 +7,18 @@ app.use(express.json())
 
 
 const categories = [
-    {id: 1, name: "Front-End Development"},
-    {id: 2, name: "Back-End Development"},
-    {id: 3, name: "Full-Stack Development"},
-    {id: 4, name: "Mobile App Development"},
-    {id: 5, name: "Machine Learning"},
-    {id: 6, name: "UI / UX Design"},
-    {id: 7, name: "Block-Chain Technology"},
-    {id: 8, name: "Cyber Security"},
-    {id: 9, name: "Operating System Design"},
+    { id: 1, name: "Front-End Development" },
+    { id: 2, name: "Back-End Development" },
+    { id: 3, name: "Full-Stack Development" },
+    { id: 4, name: "Mobile App Development" },
+    { id: 5, name: "Machine Learning" },
+    { id: 6, name: "UI / UX Design" },
+    { id: 7, name: "Block-Chain Technology" },
+    { id: 8, name: "Cyber Security" },
+    { id: 9, name: "Operating System Design" },
 ]
+
+app.get('/api/categories', (req, res) => {
+    res.send(categories);
+});
 
