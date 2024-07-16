f93f93fdf9f253e0523cba699fe2516cd2483c95
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-16 23:30:59 +0530

    Added the mongoDB server link

--- app_idx.js
+++ app_idx.js
@@ -1,6 +1,8 @@
 
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = express()
 
 const categories = require('./Routes/categories_route')
@@ -9,7 +11,11 @@ app.use(express.json())
 
 app.use(categories)
 
+mongoose.connect('mongodb://127.0.0.1/EduWebsite1DB')
+.then(() => console.log("Connection to the DB is Successful..."))
+.catch((err) => console.error(`Couldn't connect to mongoDB .... \\ ${err}`))
 
-const port = process.env.PORT || 3489;
-app.listen(port, (port) => console.log(`Listening to the port :- ${port}`));
+
+const port = process.env.PORT || 3800;
+app.listen(port, () => console.log(`Listening to the port :- ${port}`));
 
