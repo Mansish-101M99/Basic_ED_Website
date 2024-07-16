7c706339603457434a3fc676231e1af3bc4a8da8
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-16 17:45:26 +0530

    Modified route type configuration

--- Routes/categories_route.js
+++ Routes/categories_route.js
@@ -18,6 +18,12 @@ const categories = [
     { id: 9, name: "Operating System Design" },
 ]
 
+router.get('/api/categories/:id', (req, res) => {
+    const categ = categories.find( (c) => c.id === parseInt(req.params.id) );
+    if (!categ) return res.status(404).send('Category with given ID is not found..');
+    res.send(categ);
+});
+
 router.get('/api/categories', (req, res) => {
     res.send(categories);
 });
@@ -54,12 +60,6 @@ router.delete('/api/categories/:id', (req, res) => {
     res.send(categ);
 });
 
-
-router.get('/api/categories/:id', (req, res) => {
-    const categ = categories.find( (c) => c.id === parseInt(req.params.id) );
-    if (!categ) return res.status(404).send('Category with given ID is not found..');
-    res.send(categ);
-});
 
 
 function validateData(category) {
