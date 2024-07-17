1521e44114a886a87b9d512f113ff0ce0566af19
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-17 22:15:21 +0530

    Added schema to the students route

--- Routes/students_route.js
+++ Routes/students_route.js
@@ -8,7 +8,13 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 
+const studentSchema = new mongoose.Schema({
+    name: {type: String, required: true, minlength: 2, maxlength: 50},
+    isEnrolled: {type: Boolean, default: false},
+    phone_no: {type: String, required: true, minlength: 10, maxlength: 20}
+});
 
+const students = new mongoose.model('students', studentSchema);
 
 
 module.exports = router;
