28195d980fb32e0993bd4dc25758224990b3557b
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-18 13:20:44 +0530

    Added the category schema and validate function

---
+++ Models/categoryModel.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose');
+
+const Joi = require('joi');
+
+
+const categorySchema = new mongoose.Schema({
+    name: {type: String, required: true, minlength: 3, maxlength: 30}
+});
+
+const Category = new mongoose.model('Category', categorySchema);
+
+
+function validateData(category) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    // return Joi.validate(category, schema);  // Depriciated
+    return schema.validate(category);
+}
+
+
+module.Category = Category;
+module.validate = validateData;
