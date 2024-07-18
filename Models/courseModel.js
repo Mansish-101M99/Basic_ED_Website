803f0231a39c55b10deee97571d0e8717cf829af
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-18 20:19:35 +0530

    Added course schema

--- Models/courseModel.js
+++ Models/courseModel.js
@@ -4,6 +4,23 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 
+const Course = new mongoose.model('Course', new mongoose.Schema({
+    title: {type: String, required: true, trim: true, minlength: 5, maxlength: 300},
+    category: {},
+    creator: {type: String, required: true},
+    rating: {type: Number, required: true}
+}));
 
 
+function validateData(course) {
+    const schema = Joi.object({
+        title: Joi.string().min(5).max(300).required(),
+        category: Joi.string().required(),
+        creator: Joi.string().min(2).required(),
+        rating: Joi.number().required()
+    });
+}
 
+
+module.Course = Course;
+module.validating = validateData;
