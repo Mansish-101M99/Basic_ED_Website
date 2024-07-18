637d35d02986928602a39d23a9120dc24863186a
Mansish Mahakul <ultraprimemansish.100@gmail.com>
2024-07-18 21:28:40 +0530

    Added basic get, ppost and delete methods in course route

--- Routes/courses_route.js
+++ Routes/courses_route.js
@@ -3,10 +3,63 @@ const express = require('express');
 
 const router = express.Router();
 
-const {Course, validating} = require('../Models/courseModel');
+const { Course, validating } = require('../Models/courseModel');
 
 
 
+router.get('/:id', async (req, res) => {
+    const cors = await Course.findById(req.params.id);
+    if (!cors) return res.status(404).send('Student with given ID was not found..');
+    res.send(cors);
+});
+
+
+router.get('/', async (req, res) => {
+    let cors = await Course.find()
+    res.send(cors);
+});
+
+
+router.post('/', async (req, res) => {
+    const { error } = validating(req.body);
+    if (error) res.status(400).send(error.details[0].message);
+
+    const cors = new Course({
+        title: req.body.title,
+        category: {},
+        creator: req.body.creator,
+        rating: req.body.rating
+    });
+    await cors.save();
+    res.send(cors);
+});
+
+
+router.put('/:id', async (req, res) => {
+
+    const { error } = validating(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const cors = await Course.findByIdAndUpdate(req.params.id,
+        {
+            title: req.body.title,
+            category: {},
+            creator: req.body.creator,
+            rating: req.body.rating
+        },
+        { new: true });
+    if (!cors) return res.status(404).send('Student with given ID was not found..');
+
+    res.send(cors);
+});
+
+
+router.delete('/:id', async (req, res) => {
+    const cors = await Course.findByIdAndRemove(req.params.id);
+    if (!cors) return res.status(404).send('Student with given ID was not found..');
+
+    res.send(cors);
+});
 
 
 module.exports = router;
